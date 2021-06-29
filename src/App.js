5fbafa3cc0d1e74031c5dc00e17f69339163e525
cesar-vega-unosquare <cesarv@coachmehealth.org>
2021-06-29 13:14:40 -0500

    add ssh remove login

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>minimal -1</p>
+        <p>minimal +1-2</p>
       </header>
     </div>
   );
