1b507d9fdcfb2f25b3150c682d8c39200d7a443b
cesar-vega-unosquare <cesarv@coachmehealth.org>
2021-06-29 13:11:02 -0500

    remove ssh key

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>no use ssh</p>
+        <p>minimal -1</p>
       </header>
     </div>
   );
